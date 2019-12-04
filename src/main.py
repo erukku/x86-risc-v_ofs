b72e415fbb9ea9d348a092051e12da498921755b
erukku <39214747+erukku@users.noreply.github.com>
2019-12-04 22:42:53 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ magicfs = 0x10203040
 BSIZE = 1024
 DATA = 46
 IB = 32
+BB = 45
 IPB = 16
+BPB = (BSIZE*8)
 ninodes = 200
 root_inode_number = 1
 #fatal_exception_buf
@@ -19,11 +21,31 @@ NDIRECT = 12
 
 NINDIRECT = BSIZE // 4
 
-T_DIR = 1
+MAXFILE = NDIRECT + NINDIRECT
 
+T_DIR = 1
+T_FILE = 2
 T_DEV = 3
 
 root_inode = 0
+
+def divceil(x,y):
+    return (x + y - 1) // y
+
+
+
+
+
+def bfree(img,b):
+
+    
+    bi = b % BPB
+    m = 1 << (bi % 8)
+    #if ((bp[bi / 8] & m) == 0):
+    
+    img[((b)//BPB + BB)*BSIZE + (bi / 8):((b)//BPB + BB)*BSIZE + (bi / 8)+4] = (int.from_bytes( img[((b)//BPB + BB)*BSIZE + (bi / 8):((b)//BPB + BB)*BSIZE + (bi / 8)+4],"little") & ~m).to_bytes(4,'little')
+    return 0
+
 
 def bmap(img,ip,n):
     if n < NDIRECT:
@@ -50,10 +72,6 @@ def iget(img,inum):
         gg = img[(IB + inum//IPB) * 1024 + (inum % IPB) * 64:(IB + inum//IPB) * 1024 + (inum % IPB) * 64 + 64]
         return gg
 
-def geti(img,ip):
-    Ni = ninodes / IPB + 1
-    for i in range(Ni):
-        bp
 
 def skipelem(path,name):
     for i in range(len(path)):
@@ -72,6 +90,45 @@ def skipelem(path,name):
     return path,nm
 
 
+def itruncate(img,ip,size):
+    if (int.from_bytes(ip[:2],"little") == T_DEV):
+        return -1;
+    if (size > MAXFILE):
+        return -1;
+    if size < int.from_bytes(ip[8:12],"little"):
+        n = divceil(int.from_bytes(ip[8:12],"little"), BSIZE)
+        k = divceil(size, BSIZE)
+        nd = min(n,NDIRECT)
+        kd = min(k,NDIRECT)
+        for i in range(kd,nd):
+            bfree(img, int.from_bytes(ip[12 + i * 4:12 + (i + 1) * 4],"little"))
+            ip[12 + 4 * i:12 + 4 * (i+1)] = [0x00]*4
+        if n > NDIRECT:
+            iaddr = int.from_bytes(ip[12 + NDIRECT*4:12+ (NDIRECT+1)*4],"little")
+            iip = img[iaddr*BSIZE:iaddr*BSIZE + BSIZE]
+            assert(iaddr != 0)
+            ni = max(n - NDIRECT, 0);
+            ki = max(k - NDIRECT, 0);
+            for i in range(ki,ni):
+                bfree(img, iip[i:i + 4]);
+                iblock[i] = 0;
+            if ki == 0:
+                bfree(img, iaddr);
+                ip[12 + NDIRECT*4:12+ (NDIRECT+1)*4] = [0x00]*4;
+    ip[8:12] = [0x00] * 4
+    return 0;
+
+
+def ifree(img,inum):
+    ip = iget(img,inum)
+    if ip == None:
+        return -1
+    if int.from_bytes(ip[:2],"little") == 0:
+        return -1
+    if int.from_bytes(ip[4:6],"little") > 0:
+        return -1
+    img[(IB + inum//IPB) * 1024 + (inum % IPB) * 64:(IB + inum//IPB) * 1024 + (inum % IPB) * 64 + 2] = [0x00]*2
+    return 0
     
 
 def ilookup(img,rp, path):
@@ -104,6 +161,14 @@ def iread(img, dp, DE, off):
     addr = bmap(img,dp,off//DE)
     de = img[addr * BSIZE:addr * BSIZE + BSIZE]
     return de
+
+def iwrite(img, dp):
+    if int.from_bytes(dp[:2],"little") == T_DEV:
+        return -1
+    size = int.from_bytes(dp[8:12],"little")
+    addr = bmap(img,dp,off//DE)
+    img[addr * BSIZE:addr * BSIZE + BSIZE] = [0x00]*BSIZE
+    return 0
     
     
 def iunlink(img, rp, path):
@@ -115,13 +180,25 @@ def iunlink(img, rp, path):
         if name == "":
             os.error()
             return -1
-        ip = dlookup(img,rp,name,None)
+        addr,ip = dlookup(img,rp,name,None)
         if ip != None and path == "":
             if name == "." or name == "..":
                 os.error()
                 return -1
+            if iwrite(img,ip) != 0:
+                os.error()
             if int.from_bytes(ip[:2],"little") == T_DIR and dlookup(img,ip,"..",None) == rp:
-                rp[]
+                rp[4:6] = (int.from_bytes(rp[4:6],"little") - 1).to_bytes(2, 'little')
+            ip[4:6] = (int.from_bytes(ip[4:6],"little") - 1).to_bytes(2, 'little')
+            if int.from_bytes(ip[4:6],"little") == 0:
+                if int.from_bytes(ip[:2],"little") != T_DEV:
+                    itruncate(img, ip, 0)
+                ifree(img, addr)
+                return 0
+        if ip == None or int.from_bytes(rp[:2],"little") != T_DIR:
+            return -1
+        rp = ip
+
             
 
 def dlookup(img, dp,name,offp):
@@ -163,10 +240,27 @@ def o_ls(img,args):
 
 
 
-def o_get(fd,args):
+def o_get(img,args):
     if len(args) != 2:
         os.error()
         return 1
+    fspath = args[0]
+    outpath = args[1]
+
+    ip = ilookup(img, root_inode, fspath)
+    with open(outpath, "w") as f:
+        st = ""
+        for i in range(0,int.from_bytes(ip[8:12],"little"),SIZE_DE):
+            flag,de = iread(img, ip, SIZE_DE, i)
+            if int.from_bytes(de[:2],"little") == 0:
+                continue
+            for j in range(0,int.from_bytes(ip[8:12],"little"),SIZE_DE):
+                p = iget(img,int.from_bytes(de[j:2+j],"little"))
+                if p == None:
+                    break
+                st += de[2+j:16+j].decode().replace('\x00',"")
+            f.write(st)
+    return 0
 
 
 
@@ -175,16 +269,29 @@ def o_get(fd,args):
 
 
 
-def o_put(fd,args):
+
+def o_put(img,args):
     if len(args) != 2:
         os.error()
         return 1
+    fspath = args[0]
+    outpath = args[1]
+    ip = ilookup(img, root_inode, fspath)
+    with open(outpath, "r") as f:
+        if ip == None:
+            ip = icreat(img, root_inode, path, T_FILE, NULL)
+            if ip == None:
+                return -1
+        else:
+            if int.from_bytes(de[:2],"little") != T_FILE:
+                return -1
+            itruncate(img, ip, 0)
+        
 
 
 
 
-
-def o_rm(fd,args):
+def o_rm(img,args):
     if len(args) != 1:
         os.error()
         return 1
